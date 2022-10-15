be8adb956ab9ac6e5b2a9621c8b5d11c09c19ca2
sollertia <sollertia331@gmail.com>
2022-10-15 16:48:25 +0900

    First Commit

---
+++ src/main/java/com/example/springcrudapilivesession/SpringCrudApiLiveSessionApplication.java
@@ -0,0 +1,13 @@
+package com.example.springcrudapilivesession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCrudApiLiveSessionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCrudApiLiveSessionApplication.class, args);
+    }
+
+}
